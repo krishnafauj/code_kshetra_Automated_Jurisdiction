02b4d2e0527ce076151e89554899d1e7a55c9871
Soham <umbaresoham18@gmail.com>
2025-02-21 12:00:44 +0530

    Added Sidebar

---
+++ client/src/components/SidebarNavItems.jsx
@@ -0,0 +1,37 @@
+export const navItems = [
+    { name: 'Dashboard', icon: Home, path: '/' },
+    { name: 'Register Case', icon: FileText, path: '/register-case' },
+    { name: 'Search Cases', icon: Search, path: '/search' },
+    { name: 'Case Status', icon: Clock, path: '/status' },
+    { name: 'Court Calendar', icon: Calendar, path: '/calendar' },
+    { name: 'Legal Resources', icon: Scale, path: '/resources' },
+];
+
+export const advocateNavItems = [
+    { name: 'Dashboard', icon: Home, path: '/' },
+    { name: 'Register Client Case', icon: FileText, path: '/advocate-complaint' },
+    { name: 'Search Cases', icon: Search, path: '/search' },
+    { name: 'Case Status', icon: Clock, path: '/status' },
+    { name: 'Court Calendar', icon: Calendar, path: '/calendar' },
+    { name: 'Legal Resources', icon: Scale, path: '/resources' },
+];
+
+export const policeNavItems = [
+    { name: 'Dashboard', icon: Home, path: '/' },
+    { name: 'Search Cases', icon: Search, path: '/search' },
+    { name: 'View Cases', icon: FileText, path: '/police-cases' },
+    { name: 'View FIRs', icon: Clock, path: '/firs' },
+    { name: 'View Chargesheet', icon: Clock, path: '/chargesheets' },
+    { name: 'Court Calendar', icon: Calendar, path: '/calendar' },
+    { name: 'Legal Resources', icon: Scale, path: '/resources' },
+];
+
+export const magisterateNavItems = [
+    { name: 'Dashboard', icon: Home, path: '/' },
+    { name: 'Search Cases', icon: Search, path: '/search' },
+    { name: 'View Case', icon: FileText, path: '/cases' },
+    { name: 'View Chargesheet', icon: FileText, path: '/chargesheets' },
+    { name: 'Case Status', icon: Clock, path: '/status' },
+    { name: 'Court Calendar', icon: Calendar, path: '/calendar' },
+    { name: 'Legal Resources', icon: Scale, path: '/resources' },
+];
---
+++ client/src/components/SidebarStackHolders.jsx
@@ -0,0 +1,16 @@
+export const stakeholderItems = [
+    { name: 'Police Portal', icon: Building2, path: '/police' },
+    { name: 'Advocate Portal', icon: Users, path: '/advocate' },
+    { name: 'Magisterate Portal', icon: Users, path: '/magisterate' },
+];
+
+export const advocateStakeholderItems = [
+    { name: 'user Portal', icon: Users, path: '/' },
+    { name: 'Police Portal', icon: Building2, path: '/police' },
+    { name: 'Magisterate Portal', icon: Users, path: '/magisterate' },
+];
+export const magisterateStakeholderItems = [
+    { name: 'user Portal', icon: Users, path: '/' },
+    { name: 'Police Portal', icon: Building2, path: '/police' },
+    { name: 'Advocate Portal', icon: Users, path: '/advocate' },
+];
